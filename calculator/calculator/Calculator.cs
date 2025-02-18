25f5ee6e2ba2a86248517db92ff3aa809e3c6af0
Elmar Gebre Yekuno <elmayeku100@student.kau.se>
2025-02-18 19:25:37 +0100

    added logic functions

---
+++ calculator/calculator/Calculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    
+    internal class Calculator
+    {
+       public int sum = 0;
+        public int prev = 0;
+       public string prevSign = "";
+      
+       public int curr = 0;
+        public void Calculate()
+        {
+            
+
+
+                if (prevSign == "+")
+                {
+                    sum = prev + curr;
+                }
+                else if (prevSign == "-")
+                {
+                    sum = prev - curr;
+                }
+                else if (prevSign == "X")
+                {
+                    sum = prev * curr;
+                }
+                else if (prevSign == "/")
+                {
+                    if (curr == 0)
+                    {
+                        MessageBox.Show("Aja baja det går inte dela med noll!");
+                    }
+                    else
+                    {
+                        sum = prev / curr;
+                    }
+
+               
+            }
+        
+
+
+        }
+        public void Next(string prevSign2,Label label1,Label label2)
+        {
+         
+
+
+                Calculate();
+                prev = sum;
+                prevSign = prevSign2;
+                curr = 0;
+
+                label1.Text = sum.ToString();
+                label2.Text = prev.ToString() + prevSign;
+            
+
+        }
+    }
+
+   
+}
--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -2,9 +2,147 @@ namespace calculator
 {
     public partial class Form1 : Form
     {
+        Calculator calculator = new Calculator();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            calculator.Equals(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculator.curr = int.Parse(calculator.curr.ToString() + "1");
+                label1.Text = calculator.curr.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oj nu blir det så hög siffra som jag inte kan hantera :(");
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculator.curr = 0;
+                calculator.prev = 0;
+                label2.Text = "";
+                label1.Text = "0";
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            calculator.Next("+", label1, label2);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculator.curr = int.Parse(calculator.curr.ToString() + "2");
+                label1.Text = calculator.curr.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            calculator.Next("-", label1, label2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculator.curr = int.Parse(calculator.curr.ToString() + "3");
+                label1.Text = calculator.curr.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculator.curr = int.Parse(calculator.curr.ToString() + "0");
+                label1.Text = calculator.curr.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            calculator.Next("/", label1, label2);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            calculator.Next("x", label1, label2);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            calculator.Next("=", label1, label2);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculator.curr = int.Parse(calculator.curr.ToString() + "4");
+                label1.Text = calculator.curr.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculator.curr = int.Parse(calculator.curr.ToString() + "5");
+                label1.Text = calculator.curr.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculator.curr = int.Parse(calculator.curr.ToString() + "6");
+                label1.Text = calculator.curr.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
